6ed089eb1a6a527ccc97b6d3ade90d0bfb2d776c
Nadia <nadiaayalanogueira@outlook.com>
2018-12-22 12:48:49 +0400

    trying to fix the bug

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('#myform').on('submit', function(e){
+        console.log('aa');
         e.preventDefault();
         var details = $('#myform').serialize();
         $.post('data.txt', details, function(data){
